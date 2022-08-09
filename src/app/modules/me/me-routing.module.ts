04a0718ff4e8a60e265a0e46332a252e7715b396
Jorge Bobadilla Monroy <jorgeb.monroy@conecta.mx>
2022-08-08 21:24:06 -0500

    Routing básico, e integración de bootstrap

--- src/app/modules/me/me-routing.module.ts
+++ src/app/modules/me/me-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { WorksPageComponent } from './pages/works-page/works-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutPageComponent
+  },
+  {
+    path:'works',
+    component:WorksPageComponent
+  },
+  {
+    path:'**',
+    redirectTo:'about'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
